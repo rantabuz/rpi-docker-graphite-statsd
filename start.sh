d5546ff39844f4be36f16327e58eaa395923b66f
Mike Kelland <mike.kelland@gmail.com>
2018-02-28 06:09:38 +0000

    adding start script for easy re-deploys

---
+++ start.sh
@@ -0,0 +1,5 @@
+
+docker stop graphite
+docker rm graphite
+docker build --build-arg CONTAINER_TIMEZONE=Canada/Eastern -t statsd .
+docker run -d --name graphite --restart=always -p 80:80 -p 2003-2004:2003-2004 -p 2023-2024:2023-2024 -p 8125:8125/udp -p 8126:8126 statsd
